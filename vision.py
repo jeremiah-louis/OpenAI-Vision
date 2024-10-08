bc2c064cad52cbe494ad794891643c3e7a27d097
Jeremiah Louis <obobairibhojie19.jeremiah@edouniversity.edu.ng>
2024-10-08 20:34:22 +0100

    chore: default api test

---
+++ vision.py
@@ -0,0 +1,29 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+# llm_config
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
+
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What’s in this image?"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                    },
+                },
+            ],
+        }
+    ],
+    max_tokens=300,
+)
+
+print(response.choices[0])
